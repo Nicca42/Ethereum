8bbeb59a4d4d7cc543f77582f4d397d3b8c063a4
Veronica <veronica.21.coutts@gmail.com>
2019-01-18 22:10:08 +0200

    Contract skeleton added

---
+++ Projects/Xctly/test/Content.test.js
@@ -0,0 +1,30 @@
+const {
+    ether
+} = require('./helpers/ether');
+const {
+    expectThrow
+} = require('./helpers/expectThrow');
+const {
+    EVMRevert
+} = require('./helpers/EVMRevert');
+const { 
+    assertRevert 
+} = require('./helpers/assertRevert');
+
+var AccessAccount = artifacts.require("./Content.sol");
+
+contract("Content tests", function(accounts) {
+    beforeEach("", async () => {
+
+    });
+
+    describe("Basic functionality", () => {
+        it("", async () => {
+
+        });
+    });
+
+    describe("Creating Content", () => {
+
+    });
+});
